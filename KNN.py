6277da4253870413d423a56efc2ad5a4337d0408
zhoulin <Fancyzero@live.cn>
2017-02-17 17:58:45 +0800

    fix coord system

--- KNN.py
+++ KNN.py
@@ -21,9 +21,10 @@ def KNN(w,h, k,points,lbl):
     ret = np.ndarray((w,h))
     for x in range(w):
         for y in range(h):
-            uv = (y/float(h)-0.5,x/float(w)-0.5)
+            uv = (y/float(h)-0.5,(w-x)/float(w)-0.5)
             m = nearest_k(uv,k,points,lbl)
-            ret[x,y] = 0.5
+
+            ret[x,y] = m
     return ret
 
 
@@ -33,9 +34,18 @@ def KNN(w,h, k,points,lbl):
 
 
 
-training_set = np.array(((1,0),(-.5,0)))
-                   #(np.random.rand(20,2) - 0.5)*2
-training_label = np.array((0,1)) #np.random.randint(0,2,len(training_set))
+#training_set = np.array(((1,0),(-.5,0)))
+#training_label = np.random.randint(0,2,len(training_set))
+sample_count =20
+training_label = np.random.randint(0,2,sample_count)
+training_set = []
+for i in training_label:
+    if i == 0:
+        training_set.append((np.random.rand()-1,np.random.rand()*2-1))
+    else:
+        training_set.append((np.random.rand(), np.random.rand()*2-1))
+training_set = np.array(training_set)
+
 colors = list(map(lambda l: cm.rainbow(l / 2.0), training_label))
 '''
 print (training_set)
@@ -45,7 +55,7 @@ print (colors)
 
 sct = plt.scatter(training_set[:,0], training_set[:,1],color = colors)
 
-im = plt.imshow(KNN(256,256,1,training_set,training_label), animated=True , extent=[-1,1,-1,1], cmap="gray")
+im = plt.imshow(KNN(100,100,3,training_set,training_label), animated=True , extent=[-1,1,-1,1], cmap="gray", vmin = 0., vmax = 1.)
 
 
 
